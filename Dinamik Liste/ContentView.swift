02743944c6e08cb09108e87c1ec2f4264457c8fc
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-09 14:27:51 +0300

    .

--- Dinamik Liste/ContentView.swift
+++ Dinamik Liste/ContentView.swift
@@ -7,11 +7,36 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct UlkeSatirTasarimi: View {
+    var ulkeAdi:String
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       Text(ulkeAdi)
     }
+}
+
+struct ContentView: View {
+    
+    @State private var ulkelerListesi = [String]()
+    var body: some View {
+       
+        NavigationView{
+            
+            List{
+                
+                ForEach(ulkelerListesi,id:\.self){ ulke in
+                    UlkeSatirTasarimi(ulkeAdi: ulke)
+                    
+                }.onDelete(perform: ulkeSil)
+            }.navigationTitle("Ulkeler")
+        }.onAppear(){
+            self.ulkelerListesi = ["Türkiye", "Almanya", "Japonya", "Çin", "Rusya"]
+        }
+    }
+    
+    func ulkeSil(at offsets: IndexSet){
+        self.ulkelerListesi.remove(atOffsets: offsets)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
