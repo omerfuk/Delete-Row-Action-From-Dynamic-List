ce0bf59e660571ffc832fef956f939f8d11586ba
Omer Faruk Kilicaslan <omerrr_2001@hotmail.com>
2022-05-09 14:17:04 +0300

    Initial Commit

---
+++ Dinamik Liste/Dinamik_ListeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dinamik_ListeApp.swift
+//  Dinamik Liste
+//
+//  Created by Ömer Faruk Kılıçaslan on 9.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Dinamik_ListeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
